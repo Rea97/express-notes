118be023fd190503f7d9ecaa1175f4d3b3f9d8a6
Oziel Martinez <ozielrea97@hotmail.com>
2017-12-31 03:10:43 -0600

    Add home section

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  let data = ''
+  require('http').get('http://readeveloper.test/api/posts/1', response => {
+    response.on('data', chunk => data += chunk)
+
+    response.on('end', () => {
+      res.json(JSON.parse(data), 200)
+    })
+  })
+});
+
+router.get('/home', (req, res, next) => {
+  let tasks = [
+    {title: 'random task', content: 'random content', difficulty: 4, due_at: '2017-12-31 00:00:00'},
+    {title: 'random task', content: 'random content', difficulty: 4, due_at: '2017-12-31 00:00:00'},
+    {title: 'random task', content: 'random content', difficulty: 4, due_at: '2017-12-31 00:00:00'},
+    {title: 'random task', content: 'random content', difficulty: 4, due_at: '2017-12-31 00:00:00'},
+  ];
+
+  res.render('home', {tasks});
 });
 
 module.exports = router;
