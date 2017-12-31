e7a22e31d823bceacae86952f5a2487e6eeb1018
Oziel Martinez <ozielrea97@hotmail.com>
2017-12-31 03:02:23 -0600

    Add tasks routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var tasks = require('./routes/tasks');
 
 var app = express();
 
@@ -22,8 +23,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * local variables
+ */
+app.locals.appName = 'ExpressNotes'
+
+/**
+ * routes
+ */
 app.use('/', index);
 app.use('/users', users);
+app.use('/tasks', tasks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/tasks.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * Edit a task form
+ */
+router.get('/:id/edit', (req, res, next) => {
+    
+})
+
+/**
+ * Create a task form
+ */
+router.get('/create', (req, res, next) => {
+    
+})
+
+/**
+ * View a task
+ */
+router.get('/:id', (req, res, next) => {
+    
+})
+
+/**
+ * Create a new task
+ */
+router.post('/', (req, res, next) => {
+    
+})
+
+/**
+ * Update a task
+ */
+router.put('/:id', (req, res, next) => {
+    
+})
+
+
+/**
+ * Delete a task
+ */
+router.delete('/:id', (req, res, next) => {
+    
+})
+
+
+module.exports = router;
